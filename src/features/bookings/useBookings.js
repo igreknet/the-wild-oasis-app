8c013d7e4c01603b389b3283611a497c288edfd7
Ihor Nikitin <igreknet@gmail.com>
2024-08-15 12:03:26 +0300

    add sorting bookings on api side

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -13,14 +13,19 @@ export function useBookings() {
       : { field: 'status', value: filterValue };
   // : { field: 'status', value: filterValue, method: 'gte' };
 
+  //SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   //making query to supabase, to get data - queryFn: getBookings
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,10 +9,16 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   //FILTER
-  if (filter !== null) {
+  if (filter) {
     query = query[filter.method || 'eq'](filter.field, filter.value);
   }
 
+  //SORT
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === 'asc',
+    });
+  }
   const { data, error } = await query;
 
   if (error) {
